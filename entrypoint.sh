b562f86a7b0e9baf40e17d048e0333199889d795
cpliakas <cpliakas@gmail.com>
2021-05-22 15:43:27 -0400

    Added check for QUICKBASE_USER_TOKEN environment variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh -l
 
-# TODO error handling if QUICKBASE_USER_TOKEN is not set.
+if [ -z "$QUICKBASE_USER_TOKEN" ]; then
+  echo "QUICKBASE_USER_TOKEN environment variable required" >&2
+  exit 1
+fi
 
 export QUICKBASE_REALM_HOSTNAME=$1
 export QUICKBASE_APP_ID=$2
 quickbase-cli formula deploy
-
-version=$(quickbase-cli version)
-echo "::set-output name=version::$version"
