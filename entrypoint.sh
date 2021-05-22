f88781ca734b0aaadb6233987bbd72ae7635c9a4
cpliakas <cpliakas@gmail.com>
2021-05-22 14:53:26 -0400

    Testing quickbase-cli command

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh -l
 
-echo "Realm Hostname: $1"
-echo "App ID: $2"
+export QUICKBASE_REALM_HOSTNAME=$1
+export QUICKBASE_APP_ID=$2
+
+quickbase-cli app get
+
 version=$(quickbase-cli version)
 echo "::set-output name=version::$version"
