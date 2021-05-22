5bfb5e3f9abda432e53740df0d790c2e9658de08
cpliakas <cpliakas@gmail.com>
2021-05-22 15:52:03 -0400

    Removed app-id input

--- README.md
+++ README.md
@@ -7,7 +7,3 @@ This action deploys Quickbase formulas and code pages to an app.
 ### `realm-hostname`
 
 **Required** The realm hostname, e.g., `demo.quickbase.com`
-
-### `app-id`
-
-**Required** The application ID, e.g., `brcwig3fs`
--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,10 @@ if [ -z "$QUICKBASE_USER_TOKEN" ]; then
   exit 1
 fi
 
+if [ -z "$1" ]; then
+  echo "realm-hostname input required" >&2
+  exit 1
+fi
+
 export QUICKBASE_REALM_HOSTNAME=$1
-export QUICKBASE_APP_ID=$2
 quickbase-cli formula deploy
