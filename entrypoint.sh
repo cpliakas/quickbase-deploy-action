5e9a6e6b6600460d8a24562c01678b6323b139fd
cpliakas <cpliakas@gmail.com>
2021-05-22 14:59:43 -0400

    Attempt to run deploy

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,12 @@
 #!/bin/sh -l
 
+# TODO error handling if QUICKBASE_USER_TOKEN is not set.
+
 export QUICKBASE_REALM_HOSTNAME=$1
 export QUICKBASE_APP_ID=$2
 
-quickbase-cli app get
+cd $GITHUB_WORKSPACE
+quickbase-cli formula deploy
 
 version=$(quickbase-cli version)
 echo "::set-output name=version::$version"
